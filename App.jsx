68089bce92a4b72759aeb0fab99ff0c7b3b68423
jade alvares <j.alvares@nhm.ac.uk>
2017-10-19 11:23:20 +0100

    default prop(erty) values in the component constructor

--- App.jsx
+++ App.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 
 class App extends React.Component {
-   constructor(props) {
-     super(props);
-
-     this.state = {
-       header: 'header from state',
-       content: 'content from state'
-     }
-   }
-
    render() {
-     return (
-       <div>
-        <h1>{this.state.header}</h1>
-        <h2>{this.state.content}</h2>
-      </div>
-    );
-  }
+      return (
+         <div>
+            <h1>{this.props.headerProp}</h1>
+            <h2>{this.props.contentProp}</h2>
+         </div>
+      );
+   }
 }
+
+App.defaultProps = {
+   headerProp: "Header from props...",
+   contentProp:"Content from props..."
+}
+
 export default App;
