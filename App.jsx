bb9f6c068910498b0bbd8259c7c8af6bf86130cf
jade alvares <j.alvares@nhm.ac.uk>
2017-10-19 10:11:55 +0100

    JS expressions

--- App.jsx
+++ App.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 class App extends React.Component {
    render() {
+
+      var i = 1;
+
       return (
          <div>
-            Hello World!!!
+            <h1>Header</h1>
+            <h2>{i == 1 ? 'true' : 'false'}</h2>
+            <p data-myattribute = 'somevalue'>text</p>
          </div>
       );
    }
