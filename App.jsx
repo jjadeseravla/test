6fa66bb020334e8f8b94ead776d137f23c307525
jjade seravla <jadealvares@hotmail.co.uk>
2017-11-02 16:33:36 +0000

    really badly edited dino

--- App.jsx
+++ App.jsx
@@ -18,13 +18,37 @@ class App extends React.Component {
    render() {
        return (
           <div>
-             <button><img src="./images/dino.jpg" alt="dino" onClick = {this.updateState}/></button>
-             <h4>{this.state.data}</h4>
-              {/* <img src={'./images/dino.jpg'} />  */}
+            <h4>{this.state.data}</h4>
+             <button><img src="./images/smileDino.jpg" alt="dino1" onClick = {this.updateState}/></button>
+             <button><img src="./images/dino.jpg" alt="dino2" onClick = {this.updateState}/></button>
+             <button><img src="./images/dino.jpg" alt="dino3" onClick = {this.updateState}/></button>
+             <button><img src="./images/dino.jpg" alt="dino4" onClick = {this.updateState}/></button>
+             <button><img src="./images/madDino.png" alt="dino5" onClick = {this.updateState}/></button>
           </div>
        );
     }
  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //
 // class Content extends React.Component {
